566418fa7d85525c0ae9541b208e6c8f8d9b461d
D-X-Y <dxy@augmentcode.com>
2023-07-10 12:18:16 -0700

    Update Mac Setup

--- setup-machine/config-zsh.sh
+++ setup-machine/config-zsh.sh
@@ -5,6 +5,10 @@ chsh -s /bin/zsh
 echo 'change the bash to zsh'
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 echo 'install the zsh done'
+echo '--------------------'
+
+# Install coreutils to have some commands in the GNU version
+brew install coreutils
 
 if [[ `uname` == 'Darwin' ]]; then
   echo 'Mac osx copy zshrc.linux'
@@ -14,10 +18,19 @@ else
   cp zshrc.linux ~/.zshrc
 fi
 
+
+
+# Install fonts and colors
+
+mkdir -p ~/.dir_colors
+git clone https://github.com/seebi/dircolors-solarized.git
+cp dircolors-solarized/dircolors.* ~/.dir_colors/
+rm -rf dircolors-solarized
+
 mkdir -p ~/.local
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
 cp -r zsh-syntax-highlighting ~/.local
-
+rm -rf zsh-syntax-highlighting
 
 if [[ `uname` == 'Darwin' ]]; then
   echo 'Install the Powerline fonts for Mac Osx'
