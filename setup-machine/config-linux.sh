0b8d07a98099532090f7b9fb80c17f290aab6b42
D-X-Y <dxy@augment.code>
2023-07-11 18:56:46 +0000

    rename

--- setup-machine/config-linux.sh
+++ setup-machine/config-linux.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
+script_dir=$(realpath $(dirname "$0"))
+echo "The script directory is ${script_dir}"
 
 # VIM
 git clone https://github.com/altercation/solarized.git
 mkdir -p ~/.vim/colors
-cp solarized/vim-colors-solarized/colors/solarized.vim ~/.vim/colors/
-rm -rf solarized
+cp ${script_dir}/solarized/vim-colors-solarized/colors/solarized.vim ~/.vim/colors/
+rm -rf ${script_dir}/solarized
 git clone https://github.com/fugalh/desert.vim.git
 cp desert.vim/colors/desert.vim ~/.vim/colors/
-rm -rf desert.vim
-cp meta/vimrc ~/.vimrc
+rm -rf ${script_dir}/desert.vim
+cp ${script_dir}/meta/vimrc ~/.vimrc
 
 # git clone https://github.com/seebi/dircolors-solarized.git
 # cp -r dircolors-solarized ~/.dir_colors
@@ -30,3 +32,8 @@ git config --global user.email "dxy@augment.code"
 git config --global user.name "D-X-Y"
 git config --global push.default matching
 git config --global push.default simple
+
+# kubectl : https://kubernetes.io/docs/tasks/tools/install-kubectl-linux/
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+rm kubectl
