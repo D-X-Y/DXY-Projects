9725c2f3c1308e43674869cd0c61d7d60daa24fd
D-X-Y <dxy@augmentcode.com>
2023-07-11 06:07:12 +0000

    Update setups

--- setup-machine/config-vim.sh
+++ setup-machine/config-vim.sh
@@ -5,12 +5,13 @@ mkdir -p ~/.vim/colors
 cp solarized/vim-colors-solarized/colors/solarized.vim ~/.vim/colors/
 git clone https://github.com/fugalh/desert.vim.git
 cp desert.vim/colors/desert.vim ~/.vim/colors/
-cp vimrc ~/.vimrc
+cp meta/vimrc ~/.vimrc
 
-git clone https://github.com/seebi/dircolors-solarized.git
-cp -r dircolors-solarized ~/.dir_colors
+# git clone https://github.com/seebi/dircolors-solarized.git
+# cp -r dircolors-solarized ~/.dir_colors
+# rm -rf dircolors-solarized
 
-cp tmux.conf ~/.tmux.conf
+cp meta/tmux.conf ~/.tmux.conf
 
 git clone https://github.com/opensourcedesign/fonts.git
 mkdir -p ~/.fonts/freefont
