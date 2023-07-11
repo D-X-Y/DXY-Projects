2d69ae63aa83d327b5fca5497817f60d5b951d5e
D-X-Y <dxy@augmentcode.com>
2023-07-11 16:51:25 +0000

    update rm

--- setup-machine/config-vim.sh
+++ setup-machine/config-vim.sh
@@ -5,6 +5,7 @@ mkdir -p ~/.vim/colors
 cp solarized/vim-colors-solarized/colors/solarized.vim ~/.vim/colors/
 git clone https://github.com/fugalh/desert.vim.git
 cp desert.vim/colors/desert.vim ~/.vim/colors/
+rm -rf desert.vim
 cp meta/vimrc ~/.vimrc
 
 # git clone https://github.com/seebi/dircolors-solarized.git
@@ -16,6 +17,7 @@ cp meta/tmux.conf ~/.tmux.conf
 git clone https://github.com/opensourcedesign/fonts.git
 mkdir -p ~/.fonts/freefont
 cp fonts/gnu-freefont_freemono/FreeMono.ttf  ~/.fonts/freefont/
+rm -rf fonts
 
 # Git Completion System
 curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/.git-completion.bash
